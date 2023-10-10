950d3cc350a17d8db5e2cf1d84aa4751ca9692cd
babalex16 <sasha.babii@mail.ru>
2023-10-10 12:37:00 +0200

    Add project files.

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,72 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    public class PointsOfInterestController: Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointOfInterest(
+            int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest
+                .FirstOrDefault(p => p.Id == pointOfInterestId);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
+
+        [HttpPost] 
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(
+            int cityId,
+            PointOfInterestForCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if ( city == null)
+            {
+                return NotFound();
+            }
+            var maxPointOfInterestId= CitiesDataStore.Current.Cities.SelectMany(
+                        c =>c.PointsOfInterest).Max(p => p.Id);
+            //CitiesDataStore works on PointOfInterestDto instead of PointsOfInterestForCreationDto so:
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description,
+            };
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest",
+                    new
+                    {
+                        cityId = cityId,
+                        pointOfInterestId = finalPointOfInterest.Id
+                    },
+                    finalPointOfInterest );
+        }
+    }
+}
