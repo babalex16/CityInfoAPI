b94668b2234fe66256e23f0d8c60769059344deb
babalex16 <sasha.babii@mail.ru>
2023-10-10 19:24:22 +0200

    Put, Patch, Delete added

--- CityInfo.API/Models/PointOfInterestForUpdateDto.cs
+++ CityInfo.API/Models/PointOfInterestForUpdateDto.cs
@@ -2,8 +2,8 @@
 
 namespace CityInfo.API.Models
 {
-    public class PointOfInterestForCreationDto
-    {   
+    public class PointOfInterestForUpdateDto
+    {
         [Required(ErrorMessage = "You should provide a name value.")]
         [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
